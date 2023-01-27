49573c03b5ca33e943793b85f6b112b912a3be0d
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-01-27 18:11:57 +0100

    add user keygen

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import * as marketController from "./controllers/marketsController";
 import * as userController from "./controllers/userController";
-
+import userKeyGen from './services/userKeyGen'
 const router = Router();
 
 router.get("/", (req, res) => res.send("Hello World"));
 router.get("/markets", marketController.getMarkets);
-// router.get("/keyGen",);
 router.get("/user", userController.getUsers); // router.post("/updateUserData", );
+router.post("/keyGen", (req, res) => res.send(JSON.stringify(userKeyGen(req.body.wallet))));
 
 export { router };
