995a890de02c89f54cba196dfedb70dc2d5f6b9b
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-02-03 22:28:01 +0100

    add account endpoint / update oracle wallet

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,19 @@ import * as userController from "./controllers/userController";
 import userKeyGen from './services/userKeyGen'
 import userLogin from './services/userLogin'
 import createOracle from './services/createOracle'
+import uploadOracleWallet from './services/uploadOracleWallet';
+import accountInfo from './services/accountInfo';
+
+import { Account } from "aws-sdk";
 
 const router = Router();
 
 router.get("/", (req, res) => res.send("Hello World"));
-router.get("/markets", marketController.getMarkets);
+// router.get("/markets", marketController.getMarkets);
 router.get("/users", userController.getUsers); // router.post("/updateUserData", );
-router.post("/login", async (req, res) => res.send(JSON.stringify(await userLogin(req))));
+router.post("/login", async (req, res) => res.send(JSON.stringify(await userLogin(req)))); // add signature check
 router.post('/createOracle', async (req, res) => res.send(JSON.stringify(await createOracle(req))))
+router.post('/uploadOracleWallet', async (req, res) => res.send(JSON.stringify(await uploadOracleWallet(req))))
+router.get('/account', async (req, res) => res.send(JSON.stringify(await accountInfo(req))))
 export { router };
+
