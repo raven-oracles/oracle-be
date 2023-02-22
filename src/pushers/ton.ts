3f6b769821f551e65d1377dd98a3f2b7a3e6e191
sepezho ⠀ <sepezho@sepezhos-MacBook-Pro.local>
2023-02-22 21:28:28 +0100

    refactor

--- src/pushers/ton.ts
+++ src/pushers/ton.ts
@@ -7,37 +7,37 @@ export const tonPusher = async (tonPrice: number): Promise<void> => {
   let mnemonics = (process.env.TON_MNEMONIC ?? '').split(' ');
   let keypair = await mnemonicToPrivateKey(mnemonics);
 
-	const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC',{
+  const tonweb = new TonWeb(new TonWeb.HttpProvider('https://testnet.toncenter.com/api/v2/jsonRPC', {
     apiKey: "a8d8e24af2a27066b01f6362f513a29b2adacd6586a9a22af0abdecb4c9332aa"
-	}));
+  }));
 
-	let wallet = new tonweb.wallet.all["v3R2"]( tonweb.provider, {
-		publicKey:keypair.publicKey ,
-	});
+  let wallet = new tonweb.wallet.all["v3R2"](tonweb.provider, {
+    publicKey: keypair.publicKey,
+  });
 
-	const address = await wallet.getAddress();
+  const address = await wallet.getAddress();
 
   console.log(address.toString(true, true, true))
   const seqno = (await wallet.methods.seqno().call()) || 0;
 
   const toAddress = new tonweb.utils.Address(
-   'EQBUL9aM4IKaBerYv8hZP1qLgRnEPnV2DYswO0aGsywZHsVW'
-	).toString(true, true, false);
+    'EQBUL9aM4IKaBerYv8hZP1qLgRnEPnV2DYswO0aGsywZHsVW'
+  ).toString(true, true, false);
 
   try {
     const transfer = await wallet.methods.transfer({
-  		secretKey: keypair.secretKey,
-  		toAddress: toAddress,
-  		amount: TonWeb.utils.toNano('0.1'), // 0.01 TON
-  		seqno: seqno,
-  		payload: tonPrice + '',
-  		sendMode: 3,
-  	});
-  
-  	await transfer.send();
+      secretKey: keypair.secretKey,
+      toAddress: toAddress,
+      amount: TonWeb.utils.toNano('0.1'), // 0.01 TON
+      seqno: seqno,
+      payload: tonPrice + '',
+      sendMode: 3,
+    });
+
+    await transfer.send();
   } catch (e) {
     console.log(e)
-    setTimeout(()=>tonPusher(tonPrice), 10000)
+    setTimeout(() => tonPusher(tonPrice), 10000)
   }
 }
 //import { TonClient, Address, toNano, WalletContractV4, internal } from "ton";
